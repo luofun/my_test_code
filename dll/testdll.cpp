12ad100bdd15cefb255146b78ecb153d855e0ade
luofun <627018553@qq.com>
2017-09-28 00:43:26 +0800

    add lib way

--- dll/testdll.cpp
+++ dll/testdll.cpp
@@ -12,3 +12,20 @@ void main()
 	FreeLibrary(hModule);//¶¯Ì¬¿â
 	//printf("%d\n", 1 << 4);
 }
+
+#if 0
+
+#include<MakeDll.h>  
+#define DLL_API __declspec(dllimport)  
+#pragma comment(lib,"MakeDll.lib")  
+int main()  
+{  
+    Point p1, p2;  
+    p2.SetPoint(5.6f, 7.8f);  
+    p1.Display();  
+    p2.Display();  
+    cout << "5+6 = " << add(5, 6) << endl;  
+    return 0;  
+}  
+
+#endif
